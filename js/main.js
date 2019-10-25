6b66f34fda7b92899c8be981f0dd2a937d552e25
Simon Beijer <simon@Simons-MacBook-Pro-3.local>
2019-10-25 14:53:37 +0200

    Changing menu

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ let onOff = true;
 function display() {
 
     if (onOff) {
-        document.getElementById("aSide").style.width = "0";
-        document.getElementById("li").style.display = "none";
+        document.getElementById("aSide").style.width = "5rem";
+        document.getElementById("li").style.visibility = "hidden";
 
         onOff = false;
     } else {
-        document.getElementById("aSide").style.width = "40%";
-        document.getElementById("li").style.display = "flex";
+        document.getElementById("aSide").style.width = "14rem";
+        document.getElementById("li").style.visibility = "visible";
 
         onOff = true;
     }
